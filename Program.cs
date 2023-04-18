5773beb41d73720c5c5e20b614061dcb14022268
FahimNuman87 <53372580+FahimNuman@users.noreply.github.com>
2023-04-18 13:04:31 +0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,81 +1,65 @@
-﻿using CrickbazZ;
+using CrickbazZ;
 using CrickbazZ.DB;
 using Microsoft.Identity.Client;
 using System.Security.Principal;
 
 
 
+CricBuzApplicationDb cricBuzApplicationDb = new CricBuzApplicationDb();
 
-for (int id = 1; id < 2; id++)
-
+for (int id = 0; id < 10; id++)
 {
-    //==========from input part ==========
+
     Account account = new Account();
     Console.WriteLine("input Account Owners Name");
+    account.Id = id;
     account.Name = Console.ReadLine();
     account.OpeningDate = DateTime.Now;
-    CricBuzApplicationDb cricBuzApplicationDb = new CricBuzApplicationDb();
+    Console.WriteLine("input Account Owners Balance");
+    account.Balance = Convert.ToInt32(Console.ReadLine());
+
     cricBuzApplicationDb.Accounts.Add(account);
     cricBuzApplicationDb.SaveChanges();
-
-    //==========Show input List part ==========
-
-    CricBuzApplicationDb cricBuzApplicationDb1 = new CricBuzApplicationDb();
-    List<Account> accounts = new List<Account> { account };
-    accounts = cricBuzApplicationDb.Accounts.ToList();
-    foreach (Account account1 in accounts)
-    {
-        Console.WriteLine($"{account1.Id} =====>  {account1.Name}  =====>  {account1.OpeningDate}");
-
-    }
-
-    Console.WriteLine("Deposite Please:");
+}
 
 
-    //==========from deposite input part ==========
+
+
+for (int id = 0; id < 10; id++)
+{
     Deposite deposite = new Deposite();
-    Console.WriteLine("Input AccountId ");
+    Console.WriteLine("input Account Owners AccountId");
     deposite.AccountId = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Input Amount ");
+    Console.WriteLine("input Account Owners DepositeAmount");
     deposite.Amount = Convert.ToInt32(Console.ReadLine());
 
-    CricBuzApplicationDb db1 = new CricBuzApplicationDb();
-    db1.Deposites.Add(deposite);
-    db1.SaveChanges();
-    CricBuzApplicationDb db = new CricBuzApplicationDb();
-    List<Deposite> deposites = new List<Deposite> { deposite };
-    deposites = db.Deposites.ToList();
-   
-        Console.WriteLine($"{deposite.AccountId} =====>  {deposite.Amount}");
-        
-        Withdraw withdraw = new Withdraw();
-        Console.WriteLine("Input AccountId for withdrawn");
-        withdraw.AccountId = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Withdraw Amount :");
-        withdraw.Amount = Convert.ToInt32(Console.ReadLine());
 
-        CricBuzApplicationDb cricBuzApplicationDb2 = new CricBuzApplicationDb();
-        cricBuzApplicationDb2.Withdraws.Add(withdraw);
-        cricBuzApplicationDb2.SaveChanges();
+    Account account = cricBuzApplicationDb.Accounts.Find(deposite.AccountId);
+    account.Balance += deposite.Amount;
+    cricBuzApplicationDb.SaveChanges();
 
-        List<Withdraw> withdraws = new List<Withdraw>();
-        withdraws = db.Withdraws.ToList();
-        foreach (Withdraw withdraw1 in withdraws)
-        {
-            
-
-        Console.WriteLine("Balance Account:");
-            Console.WriteLine(deposite.Amount - withdraw1.Amount);
-
-        
-        Console.ReadKey();
-        }
-        Console.WriteLine(account.Name, account.OpeningDate);
+}
 
 
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("For Withdraw===Enter Account ID:");
+    int accountId = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("For Withdraw===Enter Withdrawal Amount:");
+    int amount = Convert.ToInt32(Console.ReadLine());
 
-    Console.ReadKey();
+    Withdraw withdraw = new Withdraw();
+    withdraw.AccountId = accountId;
+    withdraw.Amount = amount;
+    Account account = cricBuzApplicationDb.Accounts.Find(accountId);
+    account.Balance -= amount;
 
+    cricBuzApplicationDb.Withdraws.Add(withdraw);
+    cricBuzApplicationDb.SaveChanges();
+
+    Console.WriteLine("Account Name: " + account.Name);
+    Console.WriteLine("Opening Date: " + account.OpeningDate);
+    Console.WriteLine("Current Balance: " + account.Balance);
 }
 
 
@@ -84,6 +68,7 @@ for (int id = 1; id < 2; id++)
 
 
 Console.ReadKey();
+
 
 
 
